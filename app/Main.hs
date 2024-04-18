9842451fb3166129fda9fb2e78cabf824ad2c94f
amazingaid147 <151875992+amazingaid147@users.noreply.github.com>
2024-04-18 20:54:37 +0100

    first section finished

--- app/Main.hs
+++ app/Main.hs
@@ -1,2 +1,14 @@
 double :: Num a => a ->a
 double x = 2 * x
+
+products :: Num a => [a] -> a
+products [] = 1
+products (x:xs) = x * products xs
+
+qsort :: Ord a => [a] -> [a]
+qsort [] = []
+qsort (x:xs) =  qsort larger ++ [x] ++ qsort smaller
+                where
+                    smaller = [a | a <- xs, a <= x]
+                    larger = [a | a <- xs, a > x]
+                     
---
+++ app/a2398349-c301-409d-a3ec-c26d18e1b08f.hs
@@ -0,0 +1,2 @@
+double :: Num a => a ->a
+double x = 2 * x
